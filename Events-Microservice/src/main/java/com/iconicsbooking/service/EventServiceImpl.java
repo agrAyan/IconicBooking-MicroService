76989786f290e7867f576b582eab6e9985a1515b
samyuktha585 <78759401+samyuktha585@users.noreply.github.com>
2021-10-19 18:25:56 +0530

    Update EventServiceImpl.java

--- Events-Microservice/src/main/java/com/iconicsbooking/service/EventServiceImpl.java
+++ Events-Microservice/src/main/java/com/iconicsbooking/service/EventServiceImpl.java
@@ -33,39 +33,41 @@ public class EventServiceImpl implements IEventService{
 	@Autowired
 	RestTemplate restTemplate;
 	@Autowired
+	/*here we are injecting instance of IEventRepository interface
+         into EventServiceImpl so that we can use IEventRepository interface for accessing instance variables and methods */
 	IEventRepository eventRepository;
 	public static final String BASEURL = "http://localhost:8083/task-api";
 	public void setEventRepository(IEventRepository eventRepository) {
 		this.eventRepository = eventRepository;
 	}
-
+        //Adding event 
 	@Override
 	public Events addEvent(Events events) {
 		return eventRepository.save(events);
 	}
-
+        //update event details
 	@Override
 	public Events updateEvent(Events events) {
 		return eventRepository.save(events);		
 	}
-
+        //delete event by eventId
 	@Override
 	public void deleteEvent(int eventId) {
 		eventRepository.deleteById(eventId);		
 	}
-
+        //Getting all the events
 	@Override
 	public List<Events> getAll() {
 		return eventRepository.findAll();
 	}
-
+        //Getting specific event by using event Id
 	@Override
 	public Events getById(int serviceId) throws IdNotFoundException{
 		return eventRepository.findById(serviceId).orElseThrow(() -> new IdNotFoundException("invalid id"));
 		//return eventRepository.getById(serviceId);
 	}
 
-
+        //Getting event details based on event name
 	@Override
 	public List<Events> getByEventName(String eventName) throws EventsNotFoundException{
 		List<Events> eventList = eventRepository.findByEventName(eventName);
@@ -73,7 +75,7 @@ public class EventServiceImpl implements IEventService{
 		 throw new EventsNotFoundException("Event name not available");		
 		return eventList;
 	}
-
+        //Getting event details based on start date 
 	@Override
 	public List<Events> getByStartDate(LocalDate startDate) {
 		List<Events> eventList = eventRepository.findByStartDate(startDate);
@@ -81,7 +83,7 @@ public class EventServiceImpl implements IEventService{
 		 throw new EventsNotFoundException("On this date  not available");		
 		return eventList;
 	}
-
+        //Getting event details based on end date 
 	@Override
 	public List<Events> getByEndDate(LocalDate endDate) {
 		List<Events> eventList = eventRepository.findByEndDate(endDate);
@@ -89,7 +91,7 @@ public class EventServiceImpl implements IEventService{
 		 throw new EventsNotFoundException("On this date  not available");		
 		return eventList;
 	}
-
+        //Getting event details based on status like started or not started or in progress 
 	@Override
 	public List<Events> getByStatus(String status) {
 		Status statusValue= Status.valueOf(status);
@@ -98,7 +100,7 @@ public class EventServiceImpl implements IEventService{
 			 throw new EventsNotFoundException("Status Not available");		
 			return status1; 
 	}
-
+        //Getting event details based the price 
 	@Override
 	public List<Events> getByPrice(double price) {
 		List<Events> eventList = eventRepository.findByPriceLessThan(price);
@@ -106,33 +108,24 @@ public class EventServiceImpl implements IEventService{
 		 throw new EventsNotFoundException("In this price event not available");		
 		return eventList;
 	}
-
+        //Getting event details based on event provider  
 	@Override
 	public List<Events> getByEventProvider(String eventProvider) {
 		List<Events> eventList = eventRepository.findByEventProvider(eventProvider);
 		if(eventList.isEmpty())
 		 throw new EventsNotFoundException("Event Provider not available");		
 		return eventList;	}
-
+        //Getting event details based on priority 
 	@Override
 	public List<Events> getByPriority(String priority) {
-Priority priorityValue = Priority.valueOf(priority);
-		
+                Priority priorityValue = Priority.valueOf(priority);
 		List<Events> priority1 = eventRepository.findByPriority(priorityValue);
 		if(priority1.isEmpty())
 		 throw new EventsNotFoundException("priority not available");		
 		return priority1;
 	}
-
-
-
-
-
-	
-	
-	
-	
-	
+	/******************************Task micro-service methods******************************/
+	//Getting task details by using task Id
 	@Override
 	public Task getBytaskId(int taskId) {
 		String url = BASEURL +"/tasks/taskId/" +taskId;
@@ -141,14 +134,14 @@ Priority priorityValue = Priority.valueOf(priority);
 		return response.getBody();
 		
 	}
-
+        //Getting all the tasks
 	@Override
 	public List<Task> getAllTasks() {
 		String url = BASEURL + "/tasks";
 		ResponseEntity<List> response = restTemplate.getForEntity(url, List.class);
 		return response.getBody();
 	}
-
+        //Getting task details based on organiser name
 	@Override
 	public List<Task> getByOrganiser(String organiserName) {
 		String url = BASEURL + "/tasks/organiser/" + organiserName;
@@ -157,7 +150,7 @@ Priority priorityValue = Priority.valueOf(priority);
 		System.out.println(response.getStatusCodeValue() + "....");
 		return response.getBody();
 	}
-
+        //Getting task details by task name
 	@Override
 	public List<Task> getByTaskName(String taskName) {
 		String url = BASEURL + "/tasks/taskName/" + taskName;
@@ -166,7 +159,7 @@ Priority priorityValue = Priority.valueOf(priority);
 		System.out.println(response.getStatusCodeValue() + "....");
 		return response.getBody();
 	}
-
+        //Getting task details based on start date
 	@Override
 	public List<Task> getByTaskStartDate(LocalDate taskstartDate) {
 		String url = BASEURL + "/tasks/startDate/" + taskstartDate;
@@ -175,7 +168,7 @@ Priority priorityValue = Priority.valueOf(priority);
 		System.out.println(response.getStatusCodeValue() + "....");
 		return response.getBody();
 	}
-
+        //Getting task details based on end date
 	@Override
 	public List<Task> getByTaskEndDate(LocalDate taskendDate) {
 			String url = BASEURL + "/tasks/endDate/" + taskendDate;
@@ -184,7 +177,7 @@ Priority priorityValue = Priority.valueOf(priority);
 			System.out.println(response.getStatusCodeValue() + "....");
 			return response.getBody();
 	}
-
+        //Getting task details based on rating
 	@Override
 	public List<Task> getByRating(double rating) {
 		String url = BASEURL + "/tasks/rating/" + rating;
@@ -192,7 +185,7 @@ Priority priorityValue = Priority.valueOf(priority);
 		System.out.println(response.getStatusCodeValue() + "....");
 		return response.getBody();
 	}
-
+        //Getting task details based on status like started or not started or in progress
 	@Override
 	public List<Task> getByStatus(Status status) {
 		String url = BASEURL + "/tasks/status/" + status;
@@ -200,7 +193,7 @@ Priority priorityValue = Priority.valueOf(priority);
 		System.out.println(response.getStatusCodeValue() + "....");
 		return response.getBody();
 	}
-
+        //Getting task details based on duration
 	@Override
 	public List<Task> getByDuration(int duration) {
 		String url = BASEURL + "/tasks/duration/" + duration;
@@ -208,13 +201,9 @@ Priority priorityValue = Priority.valueOf(priority);
 		System.out.println(response.getStatusCodeValue() + "....");
 		return response.getBody();
 	}
-
+        //Assigning task in events based on Id's
 	@Override
-	public String assignTask(int eventId, int taskId) {
-
-		
-		
-		
+	public String assignTask(int eventId, int taskId) {	
 		
 		Events events = new Events();
 		events.setEventId(eventId);
@@ -222,16 +211,12 @@ Priority priorityValue = Priority.valueOf(priority);
 		System.out.println(events.getEventId());
 		String urlForTaskById= BASEURL+"/tasks/taskId/"+taskId;
 		Task taskById= restTemplate.getForEntity(urlForTaskById, Task.class).getBody();
-		System.out.println("-------------------------------------");
 		System.out.println("getByTaskId"+ taskById);
-		System.out.println("-------------------------------------");
 		taskById.setStatus(Status.IN_PROGRESS);
 		taskById.setEvents(events);
-		System.out.println("-------------------------------------");
 		System.out.println("after updating "+taskById.getEvents().getEventId());
 		//System.out.println(eventById.getIconicBooking().getCompanyId());
 		String url= BASEURL+"/tasks";
-		
 		
 		HttpHeaders requestHeaders = new HttpHeaders();
         requestHeaders.setContentType(MediaType.APPLICATION_JSON);
@@ -251,19 +236,17 @@ Priority priorityValue = Priority.valueOf(priority);
 		return "assigned task";
 		
 	}
-
+        //Releasing tasks which is completed
 	@Override
 	public String releaseTask(int taskId) {
 
 		
 		String urlForTaskById= BASEURL+"/tasks/taskId/"+taskId;
 		Task taskById= restTemplate.getForEntity(urlForTaskById, Task.class).getBody();
-		System.out.println("-------------------------------------");
 		System.out.println("getByTaskId"+ taskById);
-		System.out.println("-------------------------------------");
 		taskById.setStatus(Status.NOT_STARTED);
 		taskById.setEvents(null);
-		System.out.println("-------------------------------------");
+		
 		//System.out.println("after updating "+taskById.getEvents().getEventId());
 		//System.out.println(eventById.getIconicBooking().getCompanyId());
 		String url= BASEURL+"/tasks";
@@ -287,7 +270,7 @@ Priority priorityValue = Priority.valueOf(priority);
 	
 
 
-
+        //Adding task details in event micro service
 	@Override
 	public Task addTask(Task task) {
 		HttpHeaders httpHeader= new HttpHeaders();
